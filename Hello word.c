638a8a52e3e4115692d8abb18847ab33842c4b70
trendingvillain <113350913+trendingvillain@users.noreply.github.com>
2023-08-29 16:52:56 +0530

    Create Hello word.c

---
+++ Hello word.c
@@ -0,0 +1,24 @@
+/*This challenge requires you to print  on a single line, and then print the already provided input string to stdout. If you are not familiar with C, you may want to read about the printf() command.
+
+Example
+
+The required output is:
+
+Hello, World!  
+Life is beautiful  */
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+  	printf("Hello, World!\n%s",s);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
